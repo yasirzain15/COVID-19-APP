2f21772eae5589f69ddc43755e13eec0bca18835
yasirzain15 <softwareguy1515@gmail.com>
2025-05-22 01:12:47 +0500

    task done

--- lib/ViewModel/world_sates_view_model.dart
+++ lib/ViewModel/world_sates_view_model.dart
@@ -1,13 +1,9 @@
-
 import 'dart:convert';
-import 'package:covid_tracker/Model/countries_list_model.dart';
 import 'package:covid_tracker/Model/world_states.dart';
 import 'package:covid_tracker/ViewModel/Utilities/app_url.dart';
 import 'package:http/http.dart' as http;
 
 class WorldStatesViewModel {
-
-
   Future<WorldStatesModel> fetchWorldRecords() async {
     final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
 
@@ -20,16 +16,15 @@ class WorldStatesViewModel {
   }
 
   Future<List<dynamic>> countriesListApi() async {
-    var data ;
+    var data;
     final response = await http.get(Uri.parse(AppUrl.countriesList));
     print(response.statusCode.toString());
     print(data);
     if (response.statusCode == 200) {
-       data = jsonDecode(response.body.toString());
+      data = jsonDecode(response.body.toString());
       return data;
     } else {
       throw Exception('Error');
     }
   }
-
 }
